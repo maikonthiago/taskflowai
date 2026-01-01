8238630f6c71351ca85e9f353de4c5722b058b9f
maikonthiago <maikonthiago@github.com>
2026-01-01 03:03:25 -0300

    Fix: Force absolute path for .env loading in production config

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 from datetime import timedelta
 from dotenv import load_dotenv
 
-load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     """Configuração base"""
